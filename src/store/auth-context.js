69014a297c0747ac9458805d2f7e693c42d85a57
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-20 01:06:09 +0600

    adding

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const AuthContext = React.createContext({
 //here i wanna store token that initaially have empty string
    token: '', 
    isLoginIn: false , // i wanna tell is login use true or false
+   login:(token) => {},
+   logout:() => {}
 });
+
+const AuthContextProvider = (props) => {
+
+ const [token , setToken] = useState(null)
+
+ const userIsLoggedIn = !!token; //truethy or falsy value token is not empty it will return  true if token is empty it will return false,
+
+ const loginHandler = (tokeb) => {
+    setToken(token);
+ };
+
+ const logOutHandler =()=>{
+    setToken(null);
+ };
+
+    return <AuthContext.Provider>{props.children}</AuthContext.Provider>
+};
