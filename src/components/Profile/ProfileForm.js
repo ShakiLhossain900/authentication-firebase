e50424dd73022ae3d217b3dfa9eb93eefbfe5745
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-20 10:39:08 +0600

    adding change password

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,18 +1,43 @@
-import {useRef} 
-import classes from './ProfileForm.module.css';
+import { useRef,useContext } from "react";
+import classes from "./ProfileForm.module.css";
+import AuthContext from '../../store/auth-context';
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext)
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    //add validation
+
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBGorlsRTjc57zxjn8lKmmKfYeYIS5VSrM",{
+        method: "POST",
+        body:JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredNewPassword,
+          returnSecureToken:false
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={newPasswordInputRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
