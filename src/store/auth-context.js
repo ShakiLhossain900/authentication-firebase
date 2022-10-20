8f3f1ba8b1524a97447760e38c4683171becd18a
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-20 06:11:35 +0600

    adding

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,26 +1,32 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
 const AuthContext = React.createContext({
-//here i wanna store token that initaially have empty string
-   token: '', 
-   isLoginIn: false , // i wanna tell is login use true or false
-   login:(token) => {},
-   logout:() => {}
+    //store
+  //here i wanna store token that initaially have empty string
+  token: "",
+  isLoginIn: false, // i wanna tell is login use true or false
+  login: (token) => {},
+  logout: () => {},
 });
 
 const AuthContextProvider = (props) => {
+  const [token, setToken] = useState(null);
 
- const [token , setToken] = useState(null)
+  const userIsLoggedIn = !!token; //truethy or falsy value token is not empty it will return  true if token is empty it will return false,
 
- const userIsLoggedIn = !!token; //truethy or falsy value token is not empty it will return  true if token is empty it will return false,
-
- const loginHandler = (tokeb) => {
+  const loginHandler = (tokeb) => {
     setToken(token);
- };
+  };
 
- const logOutHandler =()=>{
+  const logOutHandler = () => {
     setToken(null);
- };
-
-    return <AuthContext.Provider>{props.children}</AuthContext.Provider>
+  };
+  const contextValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: logOutHandler,
+  };
+//auth context provider components, which can be manages of auth related state
+  return <AuthContext.Provider>{props.children}</AuthContext.Provider>;
 };
